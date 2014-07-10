b7055a6ef243fe55bb71d00eef0ccddc9c4b8a19
Nathan Woltman <nwoltman@outlook.com>
2014-07-10 01:57:05 -0700

    Version bump to 0.7.5

--- src/firebolt.js
+++ src/firebolt.js
@@ -1,6 +1,6 @@
 ﻿/**
  * Firebolt core file
- * @version 0.7.0
+ * @version 0.7.5
  * @author Nathan Woltman
  * @copyright 2014 Nathan Woltman
  * @license MIT https://github.com/FireboltJS/Firebolt/blob/master/LICENSE.txt
