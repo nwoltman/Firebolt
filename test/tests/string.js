bccd3088b7798b37509fbcd83a06ec026afec7db
Nathan Woltman <nwoltman@outlook.com>
2014-07-26 23:59:27 -0700

    Fixed up current String tests.
    Being more strict on tests for ES6 shims.

--- test/tests/string.js
+++ test/tests/string.js
@@ -12,28 +12,28 @@ test('endsWith', function() {
 	var str = 'Who am I, Gamling?';
 
 	//True
-	ok(str.endsWith('Gamling?'));				// 1
-	ok(str.endsWith('Gamling?', 99));			// 2
-	ok(str.endsWith('Gamling?', str.length));	// 3
-	ok(str.endsWith('am I', 8));				// 4
-	ok(str.endsWith('a', 5));					// 5
+	strictEqual(str.endsWith('Gamling?'), true);             // 1
+	strictEqual(str.endsWith('Gamling?', 99), true);         // 2
+	strictEqual(str.endsWith('Gamling?', str.length), true); // 3
+	strictEqual(str.endsWith('am I', 8), true);              // 4
+	strictEqual(str.endsWith('a', 5), true);                 // 5
 
 	//False
-	equal(str.endsWith('Gamling?', -1), false);	// 6
-	equal(str.endsWith('am I'), false);			// 7
-	equal(str.endsWith('am I', 9), false);		// 8
-	equal(str.endsWith('am I', 7), false);		// 9
-	equal(str.endsWith('a', 6), false);			// 10
-	equal(str.endsWith('a', 4), false);			// 11
-	equal(str.endsWith(str + 'a'), false);		// 12
-	equal(str.endsWith('?', 0), false);			// 13
+	strictEqual(str.endsWith('Gamling?', -1), false); // 6
+	strictEqual(str.endsWith('am I'), false);         // 7
+	strictEqual(str.endsWith('am I', 9), false);      // 8
+	strictEqual(str.endsWith('am I', 7), false);      // 9
+	strictEqual(str.endsWith('a', 6), false);         // 10
+	strictEqual(str.endsWith('a', 4), false);         // 11
+	strictEqual(str.endsWith(str + 'a'), false);      // 12
+	strictEqual(str.endsWith('?', 0), false);         // 13
 });
 
 test('escapeHTML', function() {
-	ok('<img src="site.com" data-a="a&b\'c" />'.escapeHTML() === '&lt;img src="site.com" data-a="a&amp;b\'c" /&gt;',
-		'escapes "<", ">", and "&"');
+	equal('<img src="site.com" data-a="a&b\'c" />'.escapeHTML(), '&lt;img src="site.com" data-a="a&amp;b\'c" /&gt;',
+		'Escapes "<", ">", and "&".');
 
-	ok('  a \n\t  '.escapeHTML() === '  a \n\t  ', 'preserves whitespace');
+	equal('  a& \n\t  '.escapeHTML(), '  a&amp; \n\t  ', 'Preserves whitespace.');
 });
 
 test('repeat', function() {
@@ -42,28 +42,25 @@ test('repeat', function() {
 	var str = 'abc';
 
 	//Test return value
-	equal(str.repeat(), '');			// 1
-	equal(str.repeat(0), '');			// 2
-	equal(str.repeat(1), 'abc');		// 3
-	equal(str.repeat(2), 'abcabc');		// 4
-	equal(str.repeat(2.5), 'abcabc');	// 5
+	strictEqual(str.repeat(), '', 'Returns an empty string when no input is given.');
+	strictEqual(str.repeat(0), '', 'Returns an empty string when given 0 as input.');
+	strictEqual(str.repeat(1), str, 'Returns the original string when given 1 as input.');
+	strictEqual(str.repeat(3), 'abcabcabc', 'Correctly repeats the string the specified number of times.');
+	strictEqual(str.repeat(2.5), 'abcabc', 'Rounds down the input number before repeating the string.');
 
 	//Test for throwing errors
-
-	// 6
 	try {
 		str.repeat(-1);
 	}
 	catch (e) {
-		ok(true);
+		ok(true, 'Thows an error when given a negative number as input.');
 	}
 
-	// 7
 	try {
 		str.repeat(1/0);
 	}
 	catch (e) {
-		ok(true);
+		ok(true, 'Thows an error when given infinity as input.');
 	}
 });
 
@@ -71,17 +68,17 @@ test('startsWith', function() {
 	var str = 'Who am I, Gamling?';
 
 	//True
-	ok(str.startsWith('Who'));		// 1
-	ok(str.startsWith('am I', 4));	// 2
+	strictEqual(str.startsWith('Who'), true);      // 1
+	strictEqual(str.startsWith('am I', 4), true);  // 2
 
 	//False
-	ok(!str.startsWith('Who are'));	// 3
-	ok(!str.startsWith('am I'));	// 4
+	strictEqual(str.startsWith('Who are'), false); // 3
+	strictEqual(str.startsWith('am I'), false);    // 4
 });
 
 test('unescapeHTML', function() {
-	ok('&lt;img src="site.com" data-a="a&amp;b\'c" /&gt;'.unescapeHTML() === '<img src="site.com" data-a="a&b\'c" />',
-		'unescapes "<", ">", and "&"');
+	equal('&lt;img src="site.com" data-a="a&amp;b\'c" /&gt;'.unescapeHTML(), '<img src="site.com" data-a="a&b\'c" />',
+		'Unescapes "<", ">", and "&".');
 
-	ok('  a \n\t  '.escapeHTML() === '  a \n\t  ', 'preserves whitespace');
+	equal('  a&amp; \n\t  '.unescapeHTML(), '  a& \n\t  ', 'Preserves whitespace.');
 });
