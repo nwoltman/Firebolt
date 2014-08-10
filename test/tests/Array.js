195db2a334e03a6059bbf28133cc06ed7d2ca0df
Nathan Woltman <nwoltman@outlook.com>
2014-08-10 02:32:44 -0700

    Unit tests for Array#equals() and Array#intersect() Plus new formatting in Array#diff() test

--- test/tests/Array.js
+++ test/tests/Array.js
@@ -63,11 +63,32 @@ test('diff', function() {
 	var array = [1, 2, 3, 4, 5];
 
 	deepEqual(array.diff(), array, 'Returns a clone when called with no parameters.');
+
 	deepEqual(array.diff([5, 2, 10]), [1, 3, 4], 'Correctly performs a set difference when given one array as input.');
+
 	deepEqual(array.diff([5, 2], [1, 4]), [3], 'Correctly performs a set difference when given multiple arrays as input.');
+
 	deepEqual((function() {
 		return array.diff(arguments);
 	})(1, 2, 5), [3, 4], 'Correctly performs a set difference when given an array-like object as input.');
+});
+
+test('equals', function() {
+	var array = [1, 2, 3];
+
+	ok(array.equals(array), 'Reports that an array equals itself.');
+
+	ok(array.equals([1, 2, 3]), 'Reports that an array equals another array with the same values.');
+
+	ok(!array.equals([3, 2, 1]), 'Reports that an array does not equal another array with the same values but in another order.');
+
+	ok(!array.equals([1, 2]), 'Reports that an array does not equal another that has a different length.');
+
+	ok((function() {
+		return array.equals(arguments);
+	})(1, 2, 3), 'Reports that an array equals another array-like object with the same values.');
+
+	throws(array.equals, TypeError, 'Throws a TypeError when executed with undefined input.');
 });
 
 test('get', function() {
@@ -81,4 +102,20 @@ test('get', function() {
 	ok(array.get(-2) === 'b', 'Get middle item with negative index.');
 	ok(array.get(-3) === 'a', 'Get first item with negative index.');
 	ok(array.get(-4) === undefined, 'Get no item with out-of-range negative index.');
+});
+
+test('intersect', function() {
+	var array = [1, 2, 3];
+
+	deepEqual(array.intersect(), array, 'Returns a clone when called with no parameters.');
+
+	deepEqual(array.intersect([2, 3, 4]), [2, 3],
+		'Correctly performs a set intersection when given one array as input.');
+
+	deepEqual(array.intersect([107, 1, 50, 2], [2, 1]), [1, 2],
+		'Correctly performs a set intersection when given multiple arrays as input.');
+
+	deepEqual((function() {
+		return array.intersect(arguments);
+	})(5, 2, 1), [1, 2], 'Correctly performs a set intersection when given an array-like object as input.');
 });
