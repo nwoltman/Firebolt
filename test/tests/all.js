4ff098f83874b360175cf9e3c00c64d3bfb2cb62
Nathan Woltman <nwoltman@outlook.com>
2014-10-07 02:16:17 -0400

    Modified Firebolt.ready() unit test to give a warning of incompleteness in IE 9/10 instead of failing the test

--- test/tests/all.js
+++ test/tests/all.js
@@ -6,17 +6,19 @@
 QUnit.config.hidepassed = true;
 
 // Setup for the Firebolt.ready() test
+window.readyTestVal = 0;
+Firebolt.ready(function() {
+	window.readyTestVal = 1;
+});
 if (document.readyState == 'loading') { // The document should not be done loading yet
-	window.readyTestVal = 0;
-	Firebolt.ready(function() {
-		window.readyTestVal = 1;
-	});
-
 	if (window.readyTestVal !== 0) {
 		test('Firebolt.ready()', function() {
-			ok(0, 'A function passed to Firebolt.ready() was called before the document became ready!');
+			ok(false, 'A function passed to Firebolt.ready() was called before the document became ready!');
 		});
 	}
+}
+else {
+	console.warn('The Firebolt.ready() test will be incomplete because the document is done loading before it should be.');
 }
 
 // Load the test modules
