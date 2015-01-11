1a2ebaa9af6358da22cd3896e892ae5673102b9f
Nathan Woltman <nwoltman@outlook.com>
2015-01-11 14:02:07 -0800

    test: Add tests for the global selector alias functions

--- test/unit/Globals.js
+++ test/unit/Globals.js
@@ -15,3 +15,37 @@ QUnit.test('FB', function(assert) {
 QUnit.test('$', function(assert) {
 	assert.equal(window.$, Firebolt, 'window.$ === Firebolt');
 });
+
+QUnit.test('$$', function(assert) {
+	assert.equal(window.$$('qunit'), document.getElementById('qunit'),
+		'Acts as an alias for document.getElementById');
+});
+
+QUnit.test('$CLS', function(assert) {
+	assert.equal(window.$CLS('class1'), document.getElementsByClassName('class1'),
+		'Acts as an alias for document.getElementsByClassName');
+});
+
+QUnit.test('$ID', function(assert) {
+	assert.equal(window.$ID, window.$$, 'Is an alias of `window.$$`.');
+});
+
+QUnit.test('$NAME', function(assert) {
+	assert.equal(window.$NAME('name1'), document.getElementsByName('name1'),
+		'Acts as an alias for document.getElementsByName');
+});
+
+QUnit.test('$QS', function(assert) {
+	assert.equal(window.$QS('#qunit'), document.querySelector('#qunit'),
+		'Acts as an alias for document.querySelector');
+});
+
+QUnit.test('$QSA', function(assert) {
+	assert.deepEqual(window.$QSA('body > div'), document.querySelectorAll('body > div'),
+		'Acts as an alias for document.querySelectorAll');
+});
+
+QUnit.test('$TAG', function(assert) {
+	assert.equal(window.$TAG('div'), document.getElementsByTagName('div'),
+		'Acts as an alias for document.getElementsByTagName');
+});
