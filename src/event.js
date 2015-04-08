bef199fe58d8bbf58844d571ae595ca5dd9f681b
Nathan Woltman <nwoltman@outlook.com>
2015-04-07 22:01:52 -0700

    event: Extend EventTarget.prototype instead of Node.prototype

--- src/event.js
+++ src/event.js
@@ -14,10 +14,17 @@
 //#region VARS
 
 var EventPrototype = Event[prototype];
+var EventTargetPrototype = EventTarget[prototype];
 var stopPropagation = EventPrototype.stopPropagation;
 
 //#endregion VARS
 
+
+/**
+ * @class EventTarget
+ * @classdesc The EventTarget interface.
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget|EventTarget - Web API Interfaces | MDN}
+ */
 
 /*
  * Calls the passed in function on each item in the enumerable.
@@ -55,14 +62,86 @@ function createEventObject(eventType, event) { // Declaring `event` in the param
 }
 
 /*
- * Simply returns `false`. For use in Node#on/off
+ * Simply returns `false`. For use in EventTarget#on/off
  */
 function returnFalse() {
   return false;
 }
 
+/* This is the function that will be invoked for each event type when a handler is set with EventTarget#on() */
+function nodeEventHandler(eventObject, extraParameters) {
+  var _this = this;
+  var target = eventObject.target;
+  var eType = eventObject.type;
+  var selectorHandlers = _this._$E_[eType];
+  var selectorHandlersCopy = {};
+  var selectors = keys(selectorHandlers).remove(''); // Don't want the selector for non-delegated handlers
+  var numSelectors = selectors.length;
+  var i = 0;
+  var selector;
+  var result;
+
+  // If the extra parameters are not defined (by `.triggerHandler()`), perhaps they were defined by `.trigger()`
+  if (extraParameters === UNDEFINED) {
+    extraParameters = eventObject._$P_;
+  }
+
+  /*
+   * @param {{f: function, d: *, o: boolean}} handlerObject - The object containing the handler data
+   *     that was set in `.on()`
+   */
+  function callHandlerOnTarget(handlerObject) {
+    eventObject.data = handlerObject.d; // Set data in the event object before calling the handler
+
+    result = handlerObject.f.call(target, eventObject, extraParameters); // Call the handler and store the result
+
+    if (result === false) {
+      eventObject.stopPropagation();
+      eventObject.preventDefault();
+    }
+
+    // Remove the handler if it should only occur once
+    if (handlerObject.o) {
+      EventTargetPrototype.off.call(_this, eType, selector, handlerObject.f);
+      handlerObject.o = 0; // Make the "one" specifier falsy so this if statement won't try to remove it again
+    }
+  }
+
+  // Only do delegated events if there are selectors that can be used to delegate events and if the target
+  // was not this element (since if it was this element there would be nothing to bubble up from)
+  if (numSelectors && target !== _this) {
+    // Build a copy of the selector handlers so they won't be altered if `.off()` is ever called
+    for (; i < numSelectors; i++) {
+      selectorHandlersCopy[selectors[i]] = arrayFrom(selectorHandlers[selectors[i]]);
+    }
+
+    // Call the handlers for each selector on each matching element
+    // up to the current element or until propagation is stopped
+    do {
+      if (isNodeElement(target)) {
+        for (i = 0; i < numSelectors; i++) {
+          if (target.matches(selector = selectors[i])) {
+            selectorHandlersCopy[selector].each(callHandlerOnTarget);
+          }
+        }
+      }
+    } while ((target = target.parentNode) !== _this && !eventObject.propagationStopped);
+  }
+
+  // If there are non-delegated handlers and propagation has not been stopped,
+  // call the handlers on the current element
+  selectorHandlers = selectorHandlers[selector = ''];
+  if (selectorHandlers && !eventObject.propagationStopped) {
+    target = _this;
+    // Use a clone so the handlers array won't be altered if `off()` is ever called
+    arrayFrom(selectorHandlers).each(callHandlerOnTarget);
+  }
+
+  return result;
+}
+
 /* 
- * Used by Node#off
+ * Used by EventTarget#off
  * Removes the passed in handler from the array of handlers or removes all handlers if handler is undefined.
  * Deletes the array of handlers if it is empty after handlers have been removed.
  */
@@ -86,21 +165,28 @@ function removeSelectorHandler(selectorHandlers, selector, handler) {
   }
 }
 
+/* Slightly alter the Event#stopPropagation() method for more convenient use in EventTarget#on() */
+EventPrototype.stopPropagation = function() {
+  this.propagationStopped = true;
+  stopPropagation.call(this);
+};
+EventPrototype.propagationStopped = false; // Set the default value on the Event prototype
+
 /**
  * Removes one or more event handlers set by `.on()` or `.one()`.
  * 
- * @function Node#off
+ * @function EventTarget#off
  * @param {String} events - One or more space-separated event types, such as "click" or "click keypress".
  * @param {String} [selector] - A selector which should match the one originally passed to `.on()`
  *     when attaching event handlers.
  * @param {Function} [handler] - A handler function previously attached for the event(s),
- *     or the special value `false` (see `Node#on()`).
+ *     or the special value `false` (see `EventTarget#on()`).
  * @see {@link http://api.jquery.com/off/#off-events-selector-handler|.off() | jQuery API Documentation}
  */
 /**
  * Removes one or more event handlers set by `.on()` or `.one()`.
  * 
- * @function Node#off
+ * @function EventTarget#off
  * @param {Object} events - An object where the string keys represent one or more space-separated event
  *     types and the values represent handler functions previously attached for the event(s).
  * @param {String} [selector] - A selector which should match the one originally passed to `.on()`
@@ -110,10 +196,10 @@ function removeSelectorHandler(selectorHandlers, selector, handler) {
 /**
  * Removes all event handlers set by `.on()` or `.one()`.
  * 
- * @function Node#off
+ * @function EventTarget#off
  * @see {@link http://api.jquery.com/off/#off|.off() | jQuery API Documentation}
  */
-NodePrototype.off = function off(events, selector, handler) {
+EventTargetPrototype.off = function off(events, selector, handler) {
   var eventHandlers = this._$E_;
   var eventType;
   var selectorHandlers;
@@ -178,85 +264,6 @@ NodePrototype.off = function off(events, selector, handler) {
   return this;
 };
 
-/* Slightly alter the Event#stopPropagation() method for more convenient use in Node#on() */
-EventPrototype.stopPropagation = function() {
-  this.propagationStopped = true;
-  stopPropagation.call(this);
-};
-EventPrototype.propagationStopped = false; // Set the default value on the Event prototype
-
-/* This is the function that will be invoked for each event type when a handler is set with Node#on() */
-function nodeEventHandler(eventObject, extraParameters) {
-  var _this = this;
-  var target = eventObject.target;
-  var eType = eventObject.type;
-  var selectorHandlers = _this._$E_[eType];
-  var selectorHandlersCopy = {};
-  var selectors = keys(selectorHandlers).remove(''); // Don't want the selector for non-delegated handlers
-  var numSelectors = selectors.length;
-  var i = 0;
-  var selector;
-  var result;
-
-  // If the extra parameters are not defined (by `.triggerHandler()`), perhaps they were defined by `.trigger()`
-  if (extraParameters === UNDEFINED) {
-    extraParameters = eventObject._$P_;
-  }
-
-  /*
-   * @param {{f: function, d: *, o: boolean}} handlerObject - The object containing the handler data
-   *     that was set in `.on()`
-   */
-  function callHandlerOnTarget(handlerObject) {
-    eventObject.data = handlerObject.d; // Set data in the event object before calling the handler
-
-    result = handlerObject.f.call(target, eventObject, extraParameters); // Call the handler and store the result
-
-    if (result === false) {
-      eventObject.stopPropagation();
-      eventObject.preventDefault();
-    }
-
-    // Remove the handler if it should only occur once
-    if (handlerObject.o) {
-      NodePrototype.off.call(_this, eType, selector, handlerObject.f);
-      handlerObject.o = 0; // Make the "one" specifier falsy so this if statement won't try to remove it again
-    }
-  }
-
-  // Only do delegated events if there are selectors that can be used to delegate events and if the target
-  // was not this element (since if it was this element there would be nothing to bubble up from)
-  if (numSelectors && target !== _this) {
-    // Build a copy of the selector handlers so they won't be altered if `.off()` is ever called
-    for (; i < numSelectors; i++) {
-      selectorHandlersCopy[selectors[i]] = arrayFrom(selectorHandlers[selectors[i]]);
-    }
-
-    // Call the handlers for each selector on each matching element
-    // up to the current element or until propagation is stopped
-    do {
-      if (isNodeElement(target)) {
-        for (i = 0; i < numSelectors; i++) {
-          if (target.matches(selector = selectors[i])) {
-            selectorHandlersCopy[selector].each(callHandlerOnTarget);
-          }
-        }
-      }
-    } while ((target = target.parentNode) !== _this && !eventObject.propagationStopped);
-  }
-
-  // If there are non-delegated handlers and propagation has not been stopped,
-  // call the handlers on the current element
-  selectorHandlers = selectorHandlers[selector = ''];
-  if (selectorHandlers && !eventObject.propagationStopped) {
-    target = _this;
-    // Use a clone so the handlers array won't be altered if `off()` is ever called
-    arrayFrom(selectorHandlers).each(callHandlerOnTarget);
-  }
-
-  return result;
-}
-
 /**
  * @summary Attaches an event handler function for one or more events to the node.
  *  
@@ -268,7 +275,7 @@ function nodeEventHandler(eventObject, extraParameters) {
  *    (with an added `data` property, and if propagation is stopped, there will be a `propagationStopped` property
  *    set to `true`).
  * 
- * @function Node#on
+ * @function EventTarget#on
  * @param {String} events - One or more space-separated event types, such as "click" or "click keypress".
  * @param {String} [selector] - A selector string to filter the descendants of the selected elements
  *     that trigger the event. If the selector is `null` or omitted, the event is always triggered
@@ -290,7 +297,7 @@ function nodeEventHandler(eventObject, extraParameters) {
  *    (with an added `data` property, and if propagation is stopped, there will be a `propagationStopped`
  *    property set to `true`).
  * 
- * @function Node#on
+ * @function EventTarget#on
  * @param {Object} events - An object where the string keys represent one or more space-separated
  * event types and the values represent handler functions to be called for the event(s).
  * @param {String} [selector] - A selector string to filter the descendants of the selected elements
@@ -299,7 +306,7 @@ function nodeEventHandler(eventObject, extraParameters) {
  * @param {*} [data] - Data to be passed to the handler in `eventObject.data` when an event is triggered.
  * @see {@link http://api.jquery.com/on/#on-events-selector-data|.on() | jQuery API Documentation}
  */
-NodePrototype.on = function on(events, selector, data, handler, /*INTERNAL*/ one) {
+EventTargetPrototype.on = function on(events, selector, data, handler, /*INTERNAL*/ one) {
   var eventHandlers = this._$E_ || (this._$E_ = {});
   var selectorIsString = typeofString(selector);
   var selectorHandlers;
@@ -358,10 +365,10 @@ NodePrototype.on = function on(events, selector, data, handler, /*INTERNAL*/ one
 /**
  * Attaches a handler to an event for the node. The handler is executed at most once per event type.
  * 
- * This fuction is the same as `Node#on()`, except the event handler is removed after it executes
+ * This fuction is the same as `EventTarget#on()`, except the event handler is removed after it executes
  * for the first time.
  * 
- * @function Node#one
+ * @function EventTarget#one
  * @param {String} events
  * @param {String} [selector]
  * @param {*} [data]
@@ -371,23 +378,23 @@ NodePrototype.on = function on(events, selector, data, handler, /*INTERNAL*/ one
 /**
  * Attaches a handler to an event for the node. The handler is executed at most once per event type.
  * 
- * This function is the same as `Node#on()`, except the event handler is removed after it executes
+ * This function is the same as `EventTarget#on()`, except the event handler is removed after it executes
  * for the first time.
  * 
- * @function Node#one
+ * @function EventTarget#one
  * @param {Object} events
  * @param {String} [selector]
  * @param {*} [data]
  * @see {@link http://api.jquery.com/one/#one-events-selector-data|.one() | jQuery API Documentation}
  */
-NodePrototype.one = function(events, selector, data, handler) {
-  return NodePrototype.on.call(this, events, selector, data, handler, 1);
+EventTargetPrototype.one = function(events, selector, data, handler) {
+  return EventTargetPrototype.on.call(this, events, selector, data, handler, 1);
 };
 
 /**
  * Triggers a real DOM event on the node for the given event type.
  * 
- * @function Node#trigger
+ * @function EventTarget#trigger
  * @param {String} eventType - A string containing a JavaScript event type, such as "click" or "submit".
  * @param {*} extraParameters - Additional parameters that will be passed as the second argument to the
  *     triggered event handler(s).
@@ -395,12 +402,12 @@ NodePrototype.one = function(events, selector, data, handler) {
 /**
  * Uses the input Event object to trigger the specified event on the node.
  * 
- * @function Node#trigger
+ * @function EventTarget#trigger
  * @param {Event} event - An {@link https://developer.mozilla.org/en-US/docs/Web/API/Event|Event} object.
  * @param {*} extraParameters - Additional parameters that will be passed as the second argument to the
  *     triggered event handler(s).
  */
-NodePrototype.trigger = function(event, extraParameters) {
+EventTargetPrototype.trigger = function(event, extraParameters) {
   if (typeofString(event)) {
     event = createEventObject(event);
   }
@@ -425,14 +432,14 @@ NodePrototype.trigger = function(event, extraParameters) {
  * + Instead of returning the node, `.triggerHandler()` returns whatever value was returned by the
  *   last handler it caused to be executed. If no handlers are triggered, it returns `undefined`.
  * 
- * @function Node#triggerHandler
+ * @function EventTarget#triggerHandler
  * @param {String} eventType - A string containing a JavaScript event type, such as "click" or "submit".
  * @param {*} extraParameters - Additional parameters that will be passed as the second argument to the
  *    triggered event handler(s).
  * @returns {?} The value returned by the event handler. If there is no handler for the specified
  *     event type, then `undefined` is returned.
  */
-NodePrototype.triggerHandler = function(event, extraParameters) {
+EventTargetPrototype.triggerHandler = function(event, extraParameters) {
   // Only trigger handlers if there are event handlers saved to the node
   return (event in this._$E_) ? nodeEventHandler.call(this, createEventObject(event), extraParameters)
                               : UNDEFINED;
@@ -466,7 +473,7 @@ NodePrototype.triggerHandler = function(event, extraParameters) {
  * @function NodeCollection#off
  * @see {@link http://api.jquery.com/off/#off|.off() | jQuery API Documentation}
  */
-NodeCollectionPrototype.off = callOnEach(NodePrototype.off);
+NodeCollectionPrototype.off = callOnEach(EventTargetPrototype.off);
 
 /**
  * @summary Attaches an event handler function for one or more events to each node in the collection.
@@ -508,7 +515,7 @@ NodeCollectionPrototype.off = callOnEach(NodePrototype.off);
  * @param {*} [data] - Data to be passed to the handler in `eventObject.data` when an event is triggered.
  * @see {@link http://api.jquery.com/on/#on-events-selector-data|.on() | jQuery API Documentation}
  */
-NodeCollectionPrototype.on = callOnEach(NodePrototype.on);
+NodeCollectionPrototype.on = callOnEach(EventTargetPrototype.on);
 
 /**
  * Attaches a handler to an event for each node in the collection.
@@ -537,7 +544,7 @@ NodeCollectionPrototype.on = callOnEach(NodePrototype.on);
  * @param {*} [data]
  * @see {@link http://api.jquery.com/one/#one-events-selector-data|.one() | jQuery API Documentation}
  */
-NodeCollectionPrototype.one = callOnEach(NodePrototype.one);
+NodeCollectionPrototype.one = callOnEach(EventTargetPrototype.one);
 
 /**
  * Triggers a real DOM event on each node in the collection for the given event type.
@@ -557,7 +564,7 @@ NodeCollectionPrototype.one = callOnEach(NodePrototype.one);
  * @param {*} extraParameters - Additional parameters that will be passed as the second argument to the
  *     triggered event handler(s).
  */
-NodeCollectionPrototype.trigger = callOnEach(NodePrototype.trigger);
+NodeCollectionPrototype.trigger = callOnEach(EventTargetPrototype.trigger);
 
 /**
  * @summary Executes all handlers attached to the node for an event type.
@@ -581,5 +588,5 @@ NodeCollectionPrototype.trigger = callOnEach(NodePrototype.trigger);
  *     event type or the NodeCollection is empty, then `undefined` is returned.
  */
 NodeCollectionPrototype.triggerHandler = function() {
-  return this[0] && NodePrototype.triggerHandler.apply(this[0], arguments);
+  return this[0] && EventTargetPrototype.triggerHandler.apply(this[0], arguments);
 };
